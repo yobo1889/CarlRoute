2e18b449c62c738e1245871b6b2dd50034d8cd96
yobo1889 <yobo1889@protonmail.com>
2022-12-09 16:17:51 -0600

    Initial Commit

---
+++ CarlRoute/CarlRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarlRouteApp.swift
+//  CarlRoute
+//
+//  Created by Yeabsira Gebreegziabher on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CarlRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarlRouteUITests/CarlRouteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CarlRouteUITestsLaunchTests.swift
+//  CarlRouteUITests
+//
+//  Created by Yeabsira Gebreegziabher on 12/9/22.
+//
+
+import XCTest
+
+final class CarlRouteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
